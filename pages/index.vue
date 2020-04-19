2ce34098ebe99a6eec94d86a18ccad62e10b2f4f
essamsaied14 <essamsaied01@gmail.com>
2020-04-19 19:28:33 +0200

    create pages-index

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <!--====== NAVBAR PART START ======-->
+    
+    <div>
+      <videos-component></videos-component>
+</div>
+
+    <!--====== NAVBAR PART ENDS ======-->
+</template>
+
+
+
+<style>
+
+</style>
+<script>
+import VideosComponent from '~/components/Videos-component.vue'
+export default {
+  components:{
+    VideosComponent
+  }
+}
+</script>
